4414047e7d9deeb9919aad6ea6fe344d5dfc15d3
FadliNur31 <fdlirmdhnn31@gmail.com>
2026-05-17 01:01:09 +0700

    Auth Done

---
+++ prisma/migrations/20260516161228_adding_user_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'SUPER_ADMIN', 'TOKO', 'DRIVER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" INTEGER,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
